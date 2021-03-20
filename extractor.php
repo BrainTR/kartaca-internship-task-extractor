23132ade9295e853ddec4a1f23daa848669e1d9b
Ömer Sert <xbraintr@gmail.com>
2021-03-20 18:15:13 +0300

    first commit

---
+++ extractor.php
@@ -0,0 +1,18 @@
+<?php
+
+$zip = new ZipArchive();
+$zip->open('kartaca.zip');
+
+$message = '';
+for ($i = 0; $i < $zip->numFiles; $i++) {
+    $filename = base64_encode($i);
+    $content = $zip->getFromName($filename);
+    
+    $bins = explode(' ', $content);
+    foreach($bins as $bin){
+        $message .= mb_chr(bindec($bin));
+    }
+}
+
+$zip->close();
+print($message);
